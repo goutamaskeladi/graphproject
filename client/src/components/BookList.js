8137df952b2834cb671aaa561e849be0c18d25d5
Goutam A S <goutamas.keladi@gmail.com>
2019-08-19 14:30:15 +0530

    Fixed CORS, Render Issue

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react'
-import { gql } from 'apollo-boost'
-import { graphql } from 'react-apollo'
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
 const getBookQuery = gql`
-     {
-         books {
-             name
-             id
-         }
-     }
-`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends Component {
-    render() {
-        console.log(this.props)
-        return (
-            <div>
-                <ul id="book-list">
-                    <li>Superman Returns</li>
-                    <li>Batman Begins</li>
-                </ul>
-            </div>
-        )
+  displayBooks = () => {
+    let { loading, books } = this.props.data;
+    if (loading) {
+      return <h4>Loading books ....</h4>;
+    } else {
+      return books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
     }
+  };
+  render() {
+    return (
+      <div>
+        <ul id="book-list">{this.displayBooks()}</ul>
+      </div>
+    );
+  }
 }
 
-export default graphql(getBookQuery)(BookList)
+export default graphql(getBookQuery)(BookList);
